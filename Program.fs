da1e64ba41fda202825149fa362ac9c06fee8b5b
ᚺᛙᛈᛜᛁᚤᛙ <nicolai.verbaarschot@gmail.com>
2021-09-10 14:12:04 +0200

    Done polynomial part 2, save for the last function 'compose'.

--- Program.fs
+++ Program.fs
@@ -1,13 +1,18 @@
 open System
 
-// 2.2
-let rec evenN n =
-  match n with
-  | 0 -> []
-  | n when n % 2 <> 0 -> (n-1)*2 :: evenN (n-1)
-  | n -> n :: evenN ((n-2)*2)
+let int2String x = sprintf "%i" x
+
+let rec toStringAccumulator (xs:list<int>) s n =
+  match xs, s, n with
+  | [], s, _ -> s
+  | x::tail, s, n -> toStringAccumulator tail (s + int2String x + "x^" + int2String n + " ") (n+1) // Must be a better way to format string
+
+let toString xs =
+  toStringAccumulator xs "p(x) is " 0
+
 
 [<EntryPoint>]
 let main argv =
-    let message = evenN 10 // Call the function
+    let message = toString [1;2;3]
+    printfn "%s" message // Call the function
     0 // return an integer exit code
--- polynomial.fsx
+++ polynomial.fsx
@@ -32,6 +32,8 @@ let rec eval a (xs:Poly) =
   | [] -> 0
   | x::tail -> x + eval a (mulC a tail)
 
+// Ask about f(+) f(-)
+
 // Part 2
 
 // Auxillary functions
@@ -41,17 +43,36 @@ let rec sum xs =
   | [] -> 0
   | x::tail -> x + sum tail
 
+let int2String x = sprintf "%i" x
+
+let rec toStringAccumulator (xs:list<int>) s n =
+  match xs, s, n with
+  | [], s, _ -> s
+  | x::tail, s, n -> toStringAccumulator tail (s + int2String x + "x^" + int2String n + " ") (n+1) // Must be a better way to format string
+
+let rec derivativeAccumulator xs n =
+  match xs, n with
+  | [], _ -> []
+  | x::tail, n -> n*x::(derivativeAccumulator tail (n+1))
+
 // Poly functions
 
 let rec isLegal xs =
   match xs with
-  | x::tail when x=0 && List.isEmpty tail -> false
-  | x::tail when x<>0 && List.isEmpty tail -> true
+  | x::tail when x=0 && ((sum tail) = 0) -> false
+  | x::tail when x<>0 && ((sum tail) = 0) -> true
   | [] -> true
   | x::tail -> isLegal tail
 
 let rec prune xs =
   match xs with
   | [] -> []
-  | x::tail when sum tail <> 0  -> x::prune tail
-  | x::tail when sum tail = 0 -> [x]
+  | x::tail when x=0 && ((sum tail) = 0)  -> [] // How to reuse isLegal?
+  | x::tail when x<>0 && ((sum tail) = 0)  -> [x]
+  | x::tail -> x::prune tail
+
+let toString xs =
+  toStringAccumulator xs "p(x) is " 0
+
+let derivative xs =
+  derivativeAccumulator xs 0
